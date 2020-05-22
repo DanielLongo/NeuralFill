1348208886f88646ce207bb381cb1f62fcc814df
Daniel Longo <dlongo@lotus.lee-messer.net>
2020-05-21 21:23:12 -0700

    conditional VAE added

---
+++ models/cVAE1c.py
@@ -0,0 +1,61 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+# basic vae from https://github.com/pytorch/examples/blob/master/vae/main.py
+
+class cVAE1c(nn.Module):
+	def __init__(self, z_dim=20):
+		super(cVAE1c, self).__init__()
+
+		self.fc1 = nn.Linear(784, 400)
+		self.fc21 = nn.Linear(400, z_dim)
+		self.fc22 = nn.Linear(400, z_dim)
+		self.fc3 = nn.Linear(z_dim * 3, 400)
+		self.fc4 = nn.Linear(400, 784)
+
+	def encode(self, x):
+		# x = torch.squeeze(x)
+		h1 = F.relu(self.fc1(x))
+		return self.fc21(h1), self.fc22(h1)
+
+	def reparameterize(self, mu, logvar):
+		std = torch.exp(0.5*logvar)
+		eps = torch.randn_like(std)
+		return mu + eps*std
+
+	def decode(self, z):
+		h3 = F.relu(self.fc3(z))
+		return torch.sigmoid(self.fc4(h3))
+
+	def forward(self, recording):
+		# x (bsize, 3, length)
+		# x[1] is the bad channel
+		# x[0] and x[2] are the good channels
+		x = recording[:, 1, :]
+		y_1 = recording[:, 0, :]
+		y_2 = recording[:, 2, :]
+
+		mu, logvar = self.encode(x.view(-1, 784))
+		z = self.reparameterize(mu, logvar)
+		
+		mu_y_0, logvar_y_0 = self.encode(y_1)
+		mu_y_1, logvar_y_1 = self.encode(y_2)
+		
+		
+		# y_0 = mu_y_0
+		# y_1 = mu_y_1
+		y_0 = self.reparameterize(mu_y_0, logvar_y_0)
+		y_1 = self.reparameterize(mu_y_1, logvar_y_1)
+		
+		y = torch.cat([y_0, y_1], 1)
+		z = torch.cat([z, y], 1)
+		
+		return self.decode(z), mu, logvar
+
+if __name__ == "__main__":
+	model = cVAE1c()
+	# sample_y = torch.zeros((64, 2, 784))
+	sample_input = torch.zeros((64, 3, 784))
+	out = model(sample_input)#, sample_y)
+	print(out[0].shape)
---
+++ models/conv_VAE.py
@@ -0,0 +1,101 @@
+# https://github.com/sksq96/pytorch-vae/blob/master/vae.py
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class Flatten(nn.Module):
+	def forward(self, input):
+		return input.view(input.size(0), -1)
+
+
+class UnFlatten(nn.Module):
+	def forward(self, input, size=256):
+		return input.view(input.size(0), size, 1, 1)
+
+
+class TransformToOriginal(nn.Module):
+	def forward(self, input):
+		return input.view(input.size(0), -1, 784)
+
+
+class Transform3D(nn.Module):
+	def forward(self, input):
+		return input.view(input.size(0), input.size(1), 28, 28)
+
+
+class ConvVAE(nn.Module):
+	def __init__(self, num_channels=1, num_channels_out=-1, h_dim=256, z_dim=32, use_cuda=torch.cuda.is_available()):
+		super(ConvVAE, self).__init__()
+
+		if num_channels_out == -1:
+			num_channels_out = num_channels
+			
+		self.use_cuda = use_cuda
+
+		self.encoder = nn.Sequential(
+			Transform3D(),
+			nn.Conv2d(num_channels, 32, kernel_size=4, stride=2),
+			nn.ReLU(),
+			nn.Conv2d(32, 64, kernel_size=4, stride=2),
+			nn.ReLU(),
+			nn.Conv2d(64, 128, kernel_size=2, stride=2),
+			nn.ReLU(),
+			nn.Conv2d(128, 256, kernel_size=2, stride=1),
+			nn.ReLU(),
+			Flatten()
+		)
+		self.fc1 = nn.Linear(h_dim, z_dim)
+		self.fc2 = nn.Linear(h_dim, z_dim)
+		self.fc3 = nn.Linear(z_dim, h_dim)
+		
+		self.decoder = nn.Sequential(
+			UnFlatten(),
+			nn.ConvTranspose2d(h_dim, 128, kernel_size=3, stride=2),
+			nn.ReLU(),
+			nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2),
+			nn.ReLU(),
+			nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),
+			nn.ReLU(),
+			nn.ConvTranspose2d(32, num_channels_out, kernel_size=6, stride=2),
+			TransformToOriginal(),
+			nn.Sigmoid(),
+		)
+		
+	def reparameterize(self, mu, logvar):
+		std = logvar.mul(0.5).exp_()
+		# return torch.normal(mu, std)
+		esp = torch.randn(*mu.size())
+		if self.use_cuda:
+			esp = esp.cuda()
+		z = mu + std * esp
+		return z
+	
+	def bottleneck(self, h):
+		mu, logvar = self.fc1(h), self.fc2(h)
+		z = self.reparameterize(mu, logvar)
+		return z, mu, logvar
+		
+
+	def forward(self, x):
+		h = self.encoder(x)
+		z, mu, logvar = self.bottleneck(h)
+		z = self.fc3(z)
+		return self.decoder(z), mu, logvar
+
+	def encode(self, x):
+		return self.bottleneck(self.encoder(x))[0]
+
+	def decode(self, z):
+		a = self.fc3(z)
+		return self.decoder(a)
+
+
+if __name__ == "__main__":
+	# x = torch.zeros(64, 1, 28, 28)
+	x = torch.zeros(64, 1, 784)
+	model = ConvVAE(num_channels=1)
+	a = model(x)
+	print("a", a[0].shape)
+
